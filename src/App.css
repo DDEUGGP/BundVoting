7717ea695cf1665b5ea2a4dcc1b02fd3c7e1bad9
DDEUGGP <ddeuggp@gmail.com>
2025-08-29 21:07:37 +0200

    Create App.css

---
+++ src/App.css
@@ -0,0 +1,31 @@
+.App {
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-header {
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.card {
+  background-color: #444;
+  border-radius: 10px;
+  padding: 1rem 2rem;
+  margin-top: 2rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.card code {
+  color: #61dafb;
+}
